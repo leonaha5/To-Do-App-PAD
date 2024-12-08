3842c401e84c0fa972eab586943044486c68ecb4
leon <leonstefanski@proton.me>
2024-12-08 19:29:46 +0100

    Done

--- ToDoList/MainWindow.axaml.cs
+++ ToDoList/MainWindow.axaml.cs
@@ -1,103 +1,86 @@
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Layout;
+using Avalonia.Media;
 
-namespace ToDoList
+namespace ToDoList;
+
+public partial class MainWindow : Window
 {
-    public partial class MainWindow : Window
+    private readonly ObservableCollection<Task> _tasks = [];
+
+    public MainWindow()
     {
-        // Kolekcja wszystkich zadań
-        private ObservableCollection<ToDoTask> tasks = new ObservableCollection<ToDoTask>();
-
-        public MainWindow()
-        {
-            InitializeComponent();
-            MyComboBox.SelectedIndex = 0;
-            MyListTasks.ItemsSource = tasks;
-        }
-
-        private void MySubmit_OnClick(object? sender, RoutedEventArgs e)
-        {
-            var description = MyTextBox.Text;
-            if (!string.IsNullOrWhiteSpace(description))
-            {
-                var task = new ToDoTask(description);
-                tasks.Add(task);
-                MyTextBox.Text = string.Empty;
-                RefreshTaskList();
-            }
-            else
-            {
-                var messageBox = new Window
-                {
-                    Title = "Błąd",
-                    Content = new TextBlock { Text = "Wpisz opis zadania." }
-                };
-                messageBox.ShowDialog(this);
-            }
-        }
-
-        private void MyDelete_OnClick(object? sender, RoutedEventArgs e)
-        {
-            if(MyListTasks.SelectedItem is ToDoTask selectedTask)
-            {
-                tasks.Remove(selectedTask);
-                RefreshTaskList();
-            }
-        }
-
-        private void MyCheckBox_Checked(object? sender, RoutedEventArgs e)
-        {
-            if (MyListTasks.SelectedItem is ToDoTask selectedTask)
-            {
-                selectedTask.IsCompleted = true;
-                RefreshTaskList();
-            }
-        }
-
-        private void MyCheckBox_Unchecked(object? sender, RoutedEventArgs e)
-        {
-            if (MyListTasks.SelectedItem is ToDoTask selectedTask)
-            {
-                selectedTask.IsCompleted = false;
-                RefreshTaskList();
-            }
-        }
-
-        private void MyComboBox_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
-        {
-            RefreshTaskList();
-        }
-
-        private void RefreshTaskList()
-        {
-            var filter = MyComboBox.SelectedIndex;
-            IEnumerable<ToDoTask> filteredTasks = filter switch
-            {
-                1 => tasks.Where(task => !task.IsCompleted), 
-                2 => tasks.Where(task => task.IsCompleted),   
-                _ => tasks                                    
-            };
-            
-            MyListTasks.ItemsSource = filteredTasks.ToList();
-        }
+        InitializeComponent();
+        FilterComboBox.SelectedIndex = 0;
+        TasksListBox.ItemsSource = _tasks;
     }
-    public class ToDoTask
-    {
-        public string Description { get; set; }
-        public bool IsCompleted { get; set; }
 
-        public ToDoTask(string description)
+    private void AddTask(object? sender, RoutedEventArgs e)
+    {
+        var description = TaskTextBox.Text;
+        if (string.IsNullOrWhiteSpace(description))
         {
-            Description = description;
-            IsCompleted = false;
+            new Window
+            {
+                Width = 150,
+                Height = 100,
+                Title = "Error",
+                Background = new SolidColorBrush(Colors.Red),
+                Content = new TextBlock
+                {
+                    Text = "Task description is required",
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center
+                }
+            }.ShowDialog(this);
+            return;
         }
-    
-        public override string ToString()
+
+        _tasks.Add(new Task(description));
+        TaskTextBox.Text = string.Empty;
+        RefreshTaskList();
+    }
+
+    private void DeleteTask(object? sender, RoutedEventArgs e)
+    {
+        if (TasksListBox.SelectedItem is not Task selectedTask) return;
+        _tasks.Remove(selectedTask);
+        RefreshTaskList();
+    }
+
+    private void MarkTaskCompleted(object? sender, RoutedEventArgs e)
+    {
+        if (TasksListBox.SelectedItem is not Task selectedTask) return;
+        selectedTask.IsCompleted = !selectedTask.IsCompleted;
+        RefreshTaskList();
+    }
+
+    private void FilterChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        RefreshTaskList();
+    }
+
+    private void RefreshTaskList()
+    {
+        TasksListBox.ItemsSource = (FilterComboBox.SelectedIndex switch
         {
-            return $"{Description} {(IsCompleted ? "(Zrobione)" : "(Do zrobienia)")}";
-        }
+            1 => _tasks.Where(task => !task.IsCompleted),
+            2 => _tasks.Where(task => task.IsCompleted),
+            _ => _tasks
+        }).ToList();
+    }
+}
+
+public class Task(string description)
+{
+    private string Description { get; } = description;
+    public bool IsCompleted { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Description}{(IsCompleted ? ": Completed" : ": Not completed")}";
     }
 }
